217e258734aff0224fd0971a76d0beaf5e02f672
Chikezie Joachim <8015657-chuddyjoachim@users.noreply.gitlab.com>
2021-12-06 10:48:36 -0700

    feat(app): add new packages, update app listening port, add graphql route

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+import { MongooseModule } from '@nestjs/mongoose';
+import { TransactionService } from './transaction/transaction.service';
+import { TransactionResolver } from './transaction/transaction.resolver';
+import { TransactionModule } from './transaction/transaction.module';
 
 @Module({
-  imports: [],
+  imports: [
+    UserModule,
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+    MongooseModule.forRoot('mongodb://localhost/nest'),
+    TransactionModule
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, TransactionService, TransactionResolver],
 })
 export class AppModule {}
+
+// 'mongodb://localhost/nestbank'
