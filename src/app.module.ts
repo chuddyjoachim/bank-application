8f2845c634c875a06c8de277ed5df17064ec946a
Chikezie Joachim <8015657-chuddyjoachim@users.noreply.gitlab.com>
2021-12-08 23:24:44 -0700

    feat(app): add ConfigService dotenv and graphql context

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,25 +1,32 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from "@nestjs/config"
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
 import { MongooseModule } from '@nestjs/mongoose';
-import { TransactionService } from './transaction/transaction.service';
-import { TransactionResolver } from './transaction/transaction.resolver';
 import { TransactionModule } from './transaction/transaction.module';
+import Ctx from './types/context.types';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
     UserModule,
     GraphQLModule.forRoot({
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      context: ({ req, res }): Ctx => ({ req, res }),
     }),
-    MongooseModule.forRoot('mongodb://localhost/nest'),
+    MongooseModule.forRoot(process.env.MONGODB_URL),
     TransactionModule
   ],
   controllers: [AppController],
-  providers: [AppService, TransactionService, TransactionResolver],
+  providers: [AppService],
 })
 export class AppModule {}
 
